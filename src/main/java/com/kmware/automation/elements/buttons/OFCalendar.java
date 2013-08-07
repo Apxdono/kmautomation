2c3b11fa901ed72b6136b201ca08b1651b09bfaf
arina.podoba <Arina.Podoba@km-ware.com>
2013-08-07 18:18:15 +0300

    Openfaces calendar changes

--- src/main/java/com/kmware/automation/elements/buttons/OFCalendar.java
+++ src/main/java/com/kmware/automation/elements/buttons/OFCalendar.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 
 import com.kmware.automation.elements.base.Element;
 import com.kmware.automation.elements.base.JQElement;
+import com.kmware.automation.enums.Months;
 import com.kmware.automation.jquery.jQuery;
 import com.kmware.automation.jquery.jQueryFactory;
 
@@ -13,7 +14,12 @@ import com.kmware.automation.jquery.jQueryFactory;
 public class OFCalendar extends Element<OFCalendar> {
     public static final String SELECTOR=".o_calendar";
     public static final String MONTH_SELECTOR="div[id$=\"%s--month\"]";
+    public static final String MONTH_INCREASE_SELECTOR=" div[id$=\"%s::year_increase\"]";
+    
     public static final String YEAR_SELECTOR=" div[id$=\"%s--year\"]";
+    public static final String YEAR_INCREASE_SELECTOR=" div[id$=\"%s::year_increase\"]";
+    public static final String YEAR_DECREASE_SELECTOR=" div[id$=\"%s::year_decrease\"]";
+//    div[id$="form:j_idt58--year
 
     public OFCalendar(jQuery j) {
         super(j);
@@ -41,16 +47,47 @@ public class OFCalendar extends Element<OFCalendar> {
     
     public OFCalendar setYear(String year){
     	String id = this.attr("id");
-    	System.out.println(this.jqf.query(String.format(YEAR_SELECTOR, id)).html(year));
+    	int currentYear = Integer.parseInt(this.jqf.query(String.format(YEAR_SELECTOR, id)).html());
+    	int selectedYear = Integer.parseInt(year);
+    	if(currentYear<selectedYear){
+    		try{
+    		this.jqf.query(String.format(YEAR_INCREASE_SELECTOR, id)).click();
+    		}catch (Exception e) {
+    			System.err.println("Error in OFCalendar");
+			}
+    		setYear(year);
+    	}else if(currentYear>selectedYear){
+    		try{
+    		this.jqf.query(String.format(YEAR_DECREASE_SELECTOR, id)).click();
+    		}catch (Exception e) {
+    			System.err.println("Error in OFCalendar");
+			}
+    		setYear(year);
+    	}
     	return this;
     }
     
-    public OFCalendar setMonth(String month){
+    public OFCalendar setMonth(int month){
     	String id = this.attr("id");
-    	System.out.println(this.jqf.query(String.format(MONTH_SELECTOR, id)).html(month));
+    	String currentMonthName = this.jqf.query(String.format(MONTH_SELECTOR, id)).html();
+    	int currentMonth = Months.valueOf(currentMonthName).ordinal();
+    	if(currentMonth<month){
+    		try{
+    		this.jqf.query(String.format(MONTH_INCREASE_SELECTOR, id)).click();
+    		}catch (Exception e) {
+    			System.err.println("Error in OFCalendar");
+			}
+    		setMonth(month);
+    	}else if(currentMonth>month){
+    		System.out.println(this.jqf.query(String.format(YEAR_INCREASE_SELECTOR, id)));
+    		try{
+    		System.out.println(this.jqf.query(String.format(YEAR_INCREASE_SELECTOR, id)).click());
+    		}catch (Exception e) {
+    			System.err.println("Error in OFCalendar");
+			}
+    		setMonth(month);
+    	}
     	return this;
     }
-    
-    //увеличение года
-    //$('div[id$="form:j_idt58::year_increase"]').click()
+  
 }
